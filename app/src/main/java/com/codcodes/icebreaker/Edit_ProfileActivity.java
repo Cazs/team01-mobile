440e431a814df092bcbe8555a9db5090b37a14e2
tevinmoodley <tevin@omegafs.co.za>
2016-07-14 16:01:52 +0200

    can select picture-Aaron

--- app/src/main/java/com/codcodes/icebreaker/Edit_ProfileActivity.java
+++ app/src/main/java/com/codcodes/icebreaker/Edit_ProfileActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
@@ -16,7 +19,11 @@ import android.widget.TextView;
 
 import com.codcodes.icebreaker.tabs.ImageConverter;
 
-public class Edit_ProfileActivity extends AppCompatActivity {
+import java.io.FileNotFoundException;
+
+public class Edit_ProfileActivity extends AppCompatActivity
+{
+    ImageView circularImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +35,44 @@ public class Edit_ProfileActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
-        Typeface h = Typeface.createFromAsset(getAssets(),"Ailerons-Typeface.otf");
+        Typeface h = Typeface.createFromAsset(getAssets(), "Ailerons-Typeface.otf");
         TextView name = (TextView) toolbar.findViewById(R.id.Edit_Heading);
         name.setTypeface(h);
 
-        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),R.drawable.seleena);
-        Bitmap circularbitmap = ImageConverter.getRoundedCornerBitMap(bitmap,100);
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.seleena);
+        Bitmap circularbitmap = ImageConverter.getRoundedCornerBitMap(bitmap, 100);
 
-        ImageView circularImageView = (ImageView) findViewById(R.id.editprofilepic);
+        circularImageView = (ImageView) findViewById(R.id.editprofilepic);
         circularImageView.setImageBitmap(circularbitmap);
 
+        Typeface EditFont = Typeface.createFromAsset(getAssets(), "Infinity.ttf");
+        TextView editphoto = (TextView) findViewById(R.id.editphoto);
+        editphoto.setTypeface(EditFont);
+
+
+        editphoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+
+            }
+        });
+
     }
+        public void onActivityResult(int requstCode,int resltCode,Intent data) {
+               super.onActivityResult(requstCode,resltCode,data);
+                Uri targetUri = data.getData();
+                Bitmap bitmap;
+                try
+                {
+                    bitmap=BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                    circularImageView.setImageBitmap(bitmap);
+                }
+                catch (FileNotFoundException e)
+                {
+                    e.printStackTrace();
+                }
+            }
 
 }
