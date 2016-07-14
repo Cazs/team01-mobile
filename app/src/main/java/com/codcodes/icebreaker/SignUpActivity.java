ec28f6f5a419c36a2770d9a63efd2705fb1b344b
tevinmoodley <tevin@omegafs.co.za>
2016-07-14 17:09:09 +0200

    validatin email and password

--- app/src/main/java/com/codcodes/icebreaker/SignUpActivity.java
+++ app/src/main/java/com/codcodes/icebreaker/SignUpActivity.java
@@ -3,30 +3,91 @@ package com.codcodes.icebreaker;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.Typeface;
+import android.media.MediaCodec;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText username;
+    EditText password;
+    EditText confirmPassword;
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Typeface heading = Typeface.createFromAsset(getAssets(),"Ailerons-Typeface.otf");
+        Typeface heading = Typeface.createFromAsset(getAssets(), "Ailerons-Typeface.otf");
         TextView headingTextView = (TextView) findViewById(R.id.SignUp);
         headingTextView.setTypeface(heading);
 
-     // EditText edit = (EditText) findViewById(R.id.email_sign_up);
-     // edit.clearFocus();
+        email = (EditText) findViewById(R.id.email_sign_up);
+        username = (EditText) findViewById(R.id.username_sign_up);
+        password = (EditText) findViewById(R.id.password_sign_up);
+        confirmPassword = (EditText) findViewById(R.id.confirm_password);
+        btnSignUp = (Button) findViewById(R.id.sign_up_button);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String e = email.getText().toString();
+                final String p = password.getText().toString();
+                if (!isValidEmail(e)) {
+                    email.setError("Invalid Email");
+                }
+                if (!isValidPassword(p)) {
+                    password.setError("Password must include:" +
+                            " \n • A minimum of 6 characters" +
+                            " \n • At least one uppercase alphabet" +
+                            " \n • One lowercase alphabet" +
+                            " \n • One number and one" +
+                            " \n • Special case character");
+                }
+            }
+        });
+    }
+
+
+    private boolean isValidEmail(String email)
+    {
+        String Email_pattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A=Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(Email_pattern);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private boolean isValidUsername(String email)
+    {
+        //// TODO: 2016/07/14
+        return true;
+    }
+
+    private boolean isValidPassword(String password)
+    {
+        String password_pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{6,}";
+
+        Pattern pattern = Pattern.compile(password_pattern);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 
 
     }
 
-}
+
+
