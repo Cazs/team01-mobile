70db6f4bbb55d20623740a216816fd858b0faf07
tevinmoodley <tevin@omegafs.co.za>
2016-07-21 14:00:11 +0200

    profile

--- app/src/main/java/com/codcodes/icebreaker/Edit_ProfileActivity.java
+++ app/src/main/java/com/codcodes/icebreaker/Edit_ProfileActivity.java
@@ -56,10 +56,9 @@ public class Edit_ProfileActivity extends AppCompatActivity
         editphoto.setTypeface(EditFont);
 
 
-
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.gender, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.gender, R.layout.spinner);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
