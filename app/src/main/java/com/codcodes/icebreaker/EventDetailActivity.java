c2d040652bb56e313187d6385a291b3575d1c75e
tevinmoodley <tevin@omegafs.co.za>
2016-07-22 13:11:32 +0200

    OTHER USERS AT EVENT

--- app/src/main/java/com/codcodes/icebreaker/EventDetailActivity.java
+++ app/src/main/java/com/codcodes/icebreaker/EventDetailActivity.java
@@ -1,22 +1,63 @@
 package com.codcodes.icebreaker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import com.codcodes.icebreaker.tabs.ImageConverter;
 
+import org.w3c.dom.Text;
+
 public class EventDetailActivity extends AppCompatActivity {
+
+    private String[] PeopleNames =
+            {
+                    "Selena Gomez",
+                    "Lindsey Morgan",
+                    "Liam Hemsworth"
+
+
+            };
+    private String[] PeopleDescrp =
+            {
+                    "They call me casanova",
+                    "Where Dreams come true",
+                    "Party in my pants..."
+
+
+            };
+    private Integer[] imgid=
+            {
+                    R.drawable.seleena,
+                    R.drawable.lindsey,
+                    R.drawable.liam
+            };
+
+    private ListView lv;
+    private ViewFlipper vf;
+    private TextView eventDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +69,7 @@ public class EventDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Bundle extras = getIntent().getExtras();
+
 
 
 
@@ -49,12 +91,29 @@ public class EventDetailActivity extends AppCompatActivity {
             eventImage.setImageBitmap(circularbitmap);
             bitmap.recycle();
 
+
+
         }
 
-        TextView eventDetails = (TextView)findViewById(R.id.Event_Heading);
+
+
+        eventDetails = (TextView)findViewById(R.id.Event_Heading);
         Typeface heading = Typeface.createFromAsset(getAssets(),"Ailerons-Typeface.otf");
         eventDetails.setTypeface(heading);
 
+        EditText accessCode = (EditText) findViewById(R.id.AccessCode);
+        accessCode.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView v, int actionID, KeyEvent event)
+            {
+                if (actionID== EditorInfo.IME_ACTION_DONE)
+                {
+                    listPeople();
+                }
+                return false;
+            }
+        });
 
 
 
@@ -67,7 +126,7 @@ public class EventDetailActivity extends AppCompatActivity {
         {
             case android.R.id.home:
                 Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 intent.putExtra("com.codcodes.icebreaker.Back",true);
                 startActivity(intent);
                 return true;
@@ -77,11 +136,44 @@ public class EventDetailActivity extends AppCompatActivity {
 
     }
 
+
+    private void listPeople()
+    {
+        //read from database
+
+        CustomListAdapter cla = new CustomListAdapter(this,PeopleNames,imgid,PeopleDescrp);
+        lv= (ListView) findViewById(R.id.contactList);
+        lv.setAdapter(cla);
+        vf = (ViewFlipper) findViewById(R.id.viewFlipper);
+        eventDetails.setText("List Of People");
+        vf.showNext();
+        lv.requestFocus();
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id)
+            {
+                // TODO Auto-generated method stub
+                String Selcteditem = PeopleNames[+position];
+                String eventDescrip = PeopleDescrp[+position];
+                int imageID = imgid[+position];
+
+
+                Intent intent = new Intent(view.getContext(),Other_Profile.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
     @Override
     public void onBackPressed()
     {
         super.onBackPressed();
         Intent i = new Intent(this,MainPageActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         i.putExtra("com.codcodes.icebreaker.Back",true);
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
