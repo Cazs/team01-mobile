fb8a134d36c8fb60948ce92e595759200d765d55
tevinmoodley <tevin@omegafs.co.za>
2016-08-09 12:33:37 +0200

    interface changes and list of people at event interface changes pop window 1 added

--- app/src/main/java/com/codcodes/icebreaker/screens/Other_Profile.java
+++ app/src/main/java/com/codcodes/icebreaker/screens/Other_Profile.java
@@ -1,5 +1,6 @@
 package com.codcodes.icebreaker.screens;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,6 +8,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -32,9 +34,13 @@ public class Other_Profile extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.pop_up_one);
+
         Bundle extras = this.getIntent().getExtras();
         if(extras!=null)
         {
+            //All this information will need to be sent to other party
             fName = extras.getString("First Name");
             LName = extras.getString("Last Name");
             Age = Integer.toString(extras.getInt("Age"));
@@ -51,7 +57,6 @@ public class Other_Profile extends AppCompatActivity
         ImageView profileImage = (ImageView) findViewById(R.id.other_pic);
         profileImage.setImageBitmap(BitmapFactory.decodeFile(ImageID));
         Button icebreak = (Button) findViewById(R.id.icebreak);
-        Typeface ib = Typeface.createFromAsset(getAssets(),"Ailerons-Typeface.otf");
         icebreak.setTypeface(heading);
 
         Typeface h = Typeface.createFromAsset(getAssets(),"Infinity.ttf");
@@ -81,7 +86,45 @@ public class Other_Profile extends AppCompatActivity
         TextView bio = (TextView) findViewById(R.id.other_profile_bio);
         bio.setTypeface(h);
         bio.setText(Bio);
+
+
+        TextView popupname = (TextView) dialog.findViewById(R.id.popup1_profile_name);
+        popupname.setText(fName+" "+LName);
+        popupname.setTypeface(h);
+
+        TextView popupage = (TextView) dialog.findViewById((R.id.popup1_profile_age)) ;
+        popupage.setText("Age:"+ Age);
+        popupage.setTypeface(h);
+
+        TextView popupgender = (TextView) dialog.findViewById((R.id.popup1_profile_gender)) ;
+        popupgender.setText(Gender);
+        popupgender.setTypeface(h);
+
+        TextView popupbiotitle = (TextView) dialog.findViewById((R.id.popup1_profile_bio_title)) ;
+        popupbiotitle.setText("Bio:");
+        popupbiotitle.setTypeface(h);
+
+        TextView popupbio = (TextView) dialog.findViewById((R.id.popup1_profile_bio)) ;
+        popupbio.setText(Bio);
+        popupbio.setTypeface(h);
+
+        Button accept = (Button) dialog.findViewById(R.id.popup1_Accept);
+        accept.setTypeface(heading);
+
+        Button reject = (Button) dialog.findViewById(R.id.popup1_Reject);
+        reject.setTypeface(heading);
+
+       icebreak.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view)
+           {
+                dialog.show();
+           }
+       });
+
     }
+
+
 
 
 }
